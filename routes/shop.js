71a81ca379f05028bd3860da715b71ea6e9801df
1kDustin <dustinschomburg94@gmail.com>
2024-02-01 08:00:42 -0600

    saving

--- routes/shop.js
+++ routes/shop.js
@@ -2,7 +2,9 @@ const express = require('express')
 
 const router = express.Router()
 
-app.get('/', (req, res, next) => {
+//.get and .post will only look for the exact match of '/'. .use will will look for any url starting with '/' depending 
+//on the order you define your middleware
+router.get('/', (req, res, next) => {
     //allows us to send a response
     res.send('<h1>Hello from express</h1>')
  })
